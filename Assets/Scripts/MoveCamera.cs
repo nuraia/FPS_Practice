6ab2374ca260b57179c479eb635aff29f1a54cee
NNahrin <61427884+nuraia@users.noreply.github.com>
2024-11-06 18:36:25 +0600

    obstacle, door done

--- Assets/Scripts/MoveCamera.cs
+++ Assets/Scripts/MoveCamera.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 public class MoveCamera : MonoBehaviour
 {
     public Transform cameraPosition;
-
+    public Vector3 offset = Vector3.zero;
     
     void LateUpdate()
     {
-        transform.position = cameraPosition.position;
+        transform.position = cameraPosition.position + offset;
         //Vector3.Lerp(transform.position, cameraPosition.position, LerpTime);
     }
 }
--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -1,7 +1,10 @@
 using System.Linq.Expressions;
+using System.Security.Cryptography;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
-
+using System.Collections.Generic;
+using System.Collections;
 public class PlayerControls : MonoBehaviour
 {
     [SerializeField] private PlayerInput m_playerInput;
@@ -9,16 +12,20 @@ public class PlayerControls : MonoBehaviour
     private InputAction jump;
     private bool IsGrounded;
     private Vector3 m_playerVelocity;
-    private float jumpHeight = 5f;
+    private float jumpHeight = 0.5f;
     private float gravityValue = -9.81f;
     private CharacterController _characterController;
+    private Transform playerCamera;
+    public MoveCamera moveCamera;
+    private Camera cam;
+    public GameObject instatiatedPanel;
     void Start()
     {
         _characterController = GetComponent<CharacterController>(); 
         m_playerInput = GetComponent<PlayerInput>();
         moveAction = m_playerInput.actions.FindAction("Movement");
         jump = m_playerInput.actions.FindAction("Jump");
-     
+        playerCamera = Camera.main.transform;
     }
 
 
@@ -38,13 +45,42 @@ public class PlayerControls : MonoBehaviour
         }
         _characterController.Move(m_playerVelocity * Time.deltaTime);
 
+        if (Input.GetMouseButtonDown(1))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                if (hit.transform.CompareTag("Obstacle"))
+                {
+                    var instatiated = Instantiate(instatiatedPanel, hit.transform);
+                    instatiated.transform.rotation = Quaternion.LookRotation(gameObject.transform.forward);
+                    StartCoroutine("PanelStay",instatiated); 
+
+                    
+                }
+            }
+        }
+    }
+
+    IEnumerator PanelStay(GameObject intantiatedPanel)
+    {
+        yield return new WaitForSeconds(3);
+        if (instatiatedPanel != null) Destroy(intantiatedPanel);
     }
 
     private void MovePlayer()
     {
         Vector2 direction = moveAction.ReadValue<Vector2>();
-        Vector3 move = new Vector3(direction.x, 0, direction.y);
-        _characterController.Move(move * Time.deltaTime);
+        Vector3 forward = playerCamera.forward;
+        Vector3 right = playerCamera.right;
+        forward.y = 0f;
+        right.y = 0f;
+        forward.Normalize();
+        right.Normalize();
+
+        Vector3 moveDirection = forward * direction.y + right * direction.x;
+        _characterController.Move(moveDirection * Time.deltaTime);
 
     }
 
@@ -52,9 +88,23 @@ public class PlayerControls : MonoBehaviour
     {
         m_playerVelocity.y += Mathf.Sqrt(jumpHeight * gravityValue * -2f);
 
-       
-       
     }
 
-   
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.CompareTag("Door"))
+        {
+            moveCamera.offset = new Vector3(1.5f, 1.2f, -1.2f);
+            moveCamera.GetComponentInChildren<LooKAroundController>().enabled = false;
+        }
+    }
+
+    private void OnCollisionExit(Collision col)
+    {
+        if (col.gameObject.CompareTag("Door"))
+        {
+            moveCamera.offset = Vector3.zero;
+            moveCamera.GetComponentInChildren<LooKAroundController>().enabled = true;
+        }
+    }
 }
