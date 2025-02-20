b1904a7d1201405ffdc3df477fa52c758092dd08
RobiulAual-Hub <61427884+nuraia@users.noreply.github.com>
2025-02-20 18:30:37 +0600

    Behavioural Graph - init

---
+++ Assets/RangeDetector.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class RangeDetector : MonoBehaviour
+{
+    [Header("Detection Setting")]
+    [SerializeField] private float detectionRadius = 10f;
+    [SerializeField] private LayerMask detectionMask;
+    [SerializeField] private bool showDebugVisuals = true;
+
+    public GameObject DetectTarget
+    {  get; 
+       set; 
+    }
+    
+    public GameObject UpdateDetector()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, detectionRadius, detectionMask);
+        if(colliders.Length > 0 )
+        {
+            DetectTarget = colliders[0].gameObject;
+        }
+        else 
+        { 
+            DetectTarget = null;
+        }
+        return DetectTarget;
+    }
+    public void OnDrawGizmos()
+    {
+        if(!showDebugVisuals || this.enabled == false) return;
+        Gizmos.color = DetectTarget ? Color.yellow : Color.red;
+        Gizmos.DrawWireSphere(transform.position, detectionRadius);
+    }
+}
